30ee7e1a11319b2fb5a6de4afe64657fc24a0f30
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-11 11:20:32 +0100

    exercice-04

--- day05/exercices/exercice-04.js
+++ day05/exercices/exercice-04.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+   function prendemail(email) {
+    if ( email.includes("@")) {
+        return true ;
+    } else {
+        return false ;
+    }
+   }
+    // console.log(prendemail("aouajyassine@gmail.com")) ;
