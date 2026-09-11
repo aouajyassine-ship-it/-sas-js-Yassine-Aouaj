3dd1b5b9a3903f0b52cbb9fa71ac107ffd7da8e0
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-11 14:24:06 +0100

    exercice-10

--- day05/exercices/exercice-10.js
+++ day05/exercices/exercice-10.js
@@ -14,4 +14,8 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+    function getdomain(email) {
+    let position = email.indexOf("@");
+    return email.slice(position + 1);
+}
+    // console.log(getdomain("contact@youcode.ma")) ;
