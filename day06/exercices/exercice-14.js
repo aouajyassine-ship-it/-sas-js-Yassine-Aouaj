4a4d96dd1e2c646ae6c7c0efa235d16e577731ce
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-14 17:32:07 +0100

    exercice-14

--- day06/exercices/exercice-14.js
+++ day06/exercices/exercice-14.js
@@ -14,4 +14,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+   let table = [1, 2, 3];
+   let dernier = table.pop();
+   table.unshift(dernier);
+console.log(table);
