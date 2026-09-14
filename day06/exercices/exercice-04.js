d66b920dcd3a1f621fd80b2eed2ec703960a96f8
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-14 12:16:05 +0100

    exercice-04

--- day06/exercices/exercice-04.js
+++ day06/exercices/exercice-04.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+    let  nombres = [10, 20, 30, 40] ;
+    let result = [];
+    for ( let i = 0 ; i < nombres.length ; i++) {
+        result.push(nombres[i] * 2);
+    }
+    console.log(result) ;
+
