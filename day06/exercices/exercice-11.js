d273c7b2fd3787d84be5155441bfe6e5d0d4fba8
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-14 17:09:07 +0100

    exercice-11

--- day06/exercices/exercice-11.js
+++ day06/exercices/exercice-11.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+ let tableau = [] ;
+ function grandtableau (tableau) {
+   for (let i = 0 ; i <= 100 ; i++){
+     tableau.push(i);
+    }
+  }
+ grandtableau(tableau);
+
