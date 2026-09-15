91bce55169e940d609f4d2cc43495fe5e9ed74cc
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-15 11:06:52 +0100

    exercice-02

--- day07/exercices/exercice-02.js
+++ day07/exercices/exercice-02.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let utilisateur = {
+    nom: "Aouaj" ,
+    age:  20 ,
+    estAdmin : true
+   };
+   utilisateur.age = 1;
+   utilisateur.gmail = "aouajyassine@gmail.com"
+   
+//    console.log(utilisateur);
