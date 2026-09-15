16ec51be81e25ecc7cd00c01d2b743c3b319b1cd
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-15 11:51:56 +0100

    exercice-06

--- day07/exercices/exercice-06.js
+++ day07/exercices/exercice-06.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+ let utilisateur = {
+    nom: "Aouaj" ,
+    age:  20 ,
+    estAdmin : true
+   };
+   console.log(Object.values(utilisateur));
