3953c160140ddc1961d529c1c7a4766667835cc6
Yassine Aouaj <aouajyassine@gmail.com>
2026-09-15 10:00:32 +0100

    guide-02

--- day07/guides/guide-02.js
+++ day07/guides/guide-02.js
@@ -9,4 +9,10 @@
  */
 'use strict';
 
-// TODO: écris ta solution ici.
+let voiture = {
+    marque : "Toyota",
+    modele : "Corolla" ,
+    annee : 2020
+ } ;
+ let cleRecherche = "modele";
+console.log(voiture[cleRecherche]);
